edf2774fd0d7b3ce2902a43073d8d13321a592b4
sidneyspe <sidneyspe@gmail.com>
2015-03-18 14:25:45 -0300

    Classes de testes agora estão com nomenclaturas em portugues, atualizado o teste de criar usuario

--- src/testes/CriarUsuarioTest.java
+++ src/testes/CriarUsuarioTest.java
@@ -21,32 +21,63 @@ public class CriarUsuarioTest {
 	@Test
 	public void testCriarUsuarioOk() throws Exception {
 
-		gerenciadorDeUsuario.criarUsuario("mark", "m@rk", "Mark Zuckerberg",	"Palo Alto, California", "mark@facebook.com");
-		Assert.assertNotNull(gerenciadorDeUsuario.usuarioBD.get("mark"));
-		Assert.assertEquals(gerenciadorDeUsuario.usuarioBD.get("mark").getLogin(),"mark");
-
-		//Assert.assertTrue(novoUsuario.getNome().contentEquals("Mark Zuckerberg"));
-		
-		
-		/*Assert.assertTrue(novoUsuario.criarUsuario("steve", "5t3v3",
-				"Steven Paul Jobs", "Palo Alto, California", "jobs@apple.com"));
-
-		Assert.assertTrue(novoUsuario.criarUsuario("bill", "severino",
+		gerenciadorDeUsuario.criarUsuario("mark", "m@rk", "Mark Zuckerberg",
+				"Palo Alto, California", "mark@facebook.com");
+		gerenciadorDeUsuario.criarUsuario("steve", "5t3v3", "Steven Paul Jobs",
+				"Palo Alto, California", "jobs@apple.com");
+		gerenciadorDeUsuario.criarUsuario("bill", "severino",
 				"William Henry Gates III", "Medina, Washington",
-				"billzin@msn.com"));
-*/
+				"billzin@msn.com");
+
+		// teste para verificar se steve foi criado
+		Assert.assertNotNull(gerenciadorDeUsuario.usuarioBD.get("steve"));
+		Assert.assertEquals(gerenciadorDeUsuario.usuarioBD.get("steve")
+				.getLogin(), "steve");
+		Assert.assertEquals(gerenciadorDeUsuario.usuarioBD.get("steve")
+				.getSenha(), "5t3v3");
+		Assert.assertEquals(gerenciadorDeUsuario.usuarioBD.get("steve")
+				.getNome(), "Steven Paul Jobs");
+		Assert.assertEquals(gerenciadorDeUsuario.usuarioBD.get("steve")
+				.getEndereco(), "Palo Alto, California");
+		Assert.assertEquals(gerenciadorDeUsuario.usuarioBD.get("steve")
+				.getEmail(), "jobs@apple.com");
+
+		// teste para verificar se mark foi criado
+		Assert.assertNotNull(gerenciadorDeUsuario.usuarioBD.get("mark"));
+		Assert.assertEquals(gerenciadorDeUsuario.usuarioBD.get("mark")
+				.getLogin(), "mark");
+		Assert.assertEquals(gerenciadorDeUsuario.usuarioBD.get("mark")
+				.getSenha(), "m@rk");
+		Assert.assertEquals(gerenciadorDeUsuario.usuarioBD.get("mark")
+				.getNome(), "Mark Zuckerberg");
+		Assert.assertEquals(gerenciadorDeUsuario.usuarioBD.get("mark")
+				.getEndereco(), "Palo Alto, California");
+		Assert.assertEquals(gerenciadorDeUsuario.usuarioBD.get("mark")
+				.getEmail(), "mark@facebook.com");
+
+		// teste para verificar se bill foi criado
+		Assert.assertNotNull(gerenciadorDeUsuario.usuarioBD.get("bill"));
+		Assert.assertEquals(gerenciadorDeUsuario.usuarioBD.get("bill")
+				.getLogin(), "bill");
+		Assert.assertEquals(gerenciadorDeUsuario.usuarioBD.get("bill")
+				.getSenha(), "severino");
+		Assert.assertEquals(gerenciadorDeUsuario.usuarioBD.get("bill")
+				.getNome(), "William Henry Gates III");
+		Assert.assertEquals(gerenciadorDeUsuario.usuarioBD.get("bill")
+				.getEndereco(), "Medina, Washington");
+		Assert.assertEquals(gerenciadorDeUsuario.usuarioBD.get("bill")
+				.getEmail(), "billzin@msn.com");
+
 	}
 
-	@Test (expected=Exception.class)
+	@Test(expected = Exception.class)
 	public void testLoginInvalidoCriarUsuario() throws Exception {
 
-		
-		gerenciadorDeUsuario.criarUsuario(null, "xptz", "xpto", "xpto", "logininvalido@gmail.com");
-		
+		gerenciadorDeUsuario.criarUsuario(null, "xptz", "xpto", "xpto",
+				"logininvalido@gmail.com");
 
-		gerenciadorDeUsuario.criarUsuario("", "xptz", "xpto", "xpto", "deuerro@gmail.com");
-		
-		
+		gerenciadorDeUsuario.criarUsuario("", "xptz", "xpto", "xpto",
+				"deuerro@gmail.com");
 
 	}
 
--- src/testes/ValidarEmailTest.java
+++ src/testes/ValidarEmailTest.java
@@ -6,16 +6,16 @@ import org.junit.Test;
 
 import control.ValidadorDeStrings;
 
-public class EmailValidatorTest {
+public class ValidarEmailTest {
 
-	private ValidadorDeStrings strValidator;
-	String[] validEmailProvider = new String[] { "mkyong@yahoo.com",
+	private ValidadorDeStrings strValidador;
+	String[] emailsValidos = new String[] { "mkyong@yahoo.com",
 			"mkyong-100@yahoo.com", "mkyong.100@yahoo.com",
 			"mkyong111@mkyong.com", "mkyong-100@mkyong.net",
 			"mkyong.100@mkyong.com.au", "mkyong@1.com", "mkyong@gmail.com.com",
 			"mkyong+100@gmail.com", "mkyong-100@yahoo-test.com" };
 
-	String[] invalidEmailProvider = new String[] { "mkyong", "mkyong@.com.my",
+	String[] emailsInvalidos = new String[] { "mkyong", "mkyong@.com.my",
 			"mkyong123@gmail.a", "mkyong123@.com", "mkyong123@.com.com",
 			".mkyong@mkyong.com", "mkyong()*@gmail.com", "mkyong@%*.com",
 			"mkyong..2002@gmail.com", "mkyong.@gmail.com",
@@ -23,14 +23,14 @@ public class EmailValidatorTest {
 
 	@Before
 	public void setUp() {
-		strValidator = new ValidadorDeStrings();
+		strValidador = new ValidadorDeStrings();
 	}
 
 	@Test
 	public void ValidEmailTest() {
 
-		for (String temp : validEmailProvider) {
-			boolean valid = strValidator.validarEmail(temp);
+		for (String temp : emailsValidos) {
+			boolean valid = strValidador.validarEmail(temp);
 			//System.out.println("Email is valid : " + temp + " , " + valid);
 			Assert.assertEquals(valid, true);
 		}
@@ -40,8 +40,8 @@ public class EmailValidatorTest {
 	@Test
 	public void InValidEmailTest() {
 
-		for (String temp : invalidEmailProvider) {
-			boolean valid = strValidator.validarEmail(temp);
+		for (String temp : emailsInvalidos) {
+			boolean valid = strValidador.validarEmail(temp);
 			//System.out.println("Email is valid : " + temp + " , " + valid);
 			Assert.assertEquals(valid, false);
 		}
